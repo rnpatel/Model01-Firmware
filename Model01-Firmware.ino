01a844b3075b55d86810c9563852a1a93d02785d
Rajiv Patel <rpatel@voalte.com>
2018-02-20 16:50:43 -0500

    master
    - replaced the right-index escape key with backslash ; having backslash and pipe
      on the function layer was bullshit
    - added #define for KEYCHORD_HYPER and KEYCHORD_MEH
      - KEYCHORD_HYPER is ⇧⌘⌥^
      - KEYCHORD_MEH is ⇧⌘⌥

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -6,6 +6,9 @@
 #define BUILD_INFORMATION "locally built"
 #endif
 
+#define KEYCHORD_HYPER LSHIFT(LALT(LGUI(Key_LeftControl)))
+#define KEYCHORD_MEH   LSHIFT(LALT(Key_LeftGui))
+
 
 /**
  * These #include directives pull in the Kaleidoscope firmware core,
@@ -146,19 +149,19 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
       Key_Escape,      Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
       Key_Backtick,    Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
       Key_LeftControl, Key_A, Key_S, Key_D, Key_F, Key_G,
-      Key_LeftShift,   Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
+      Key_LeftShift,   Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Backslash,
 
-      Key_LeftGui, Key_LeftControl, Key_LeftAlt, LALT(LGUI(Key_LeftControl)),
+      Key_LeftGui, Key_LeftControl, Key_LeftAlt, KEYCHORD_MEH,
 
       ShiftToLayer(FUNCTION),
 
       //
       // right hand
       //
-      LSHIFT(LALT(LGUI(Key_LeftControl))), Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_Backspace,
-      Key_Enter,                           Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
-                                           Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-      Key_Backspace,                       Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
+      KEYCHORD_HYPER, Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_Backspace,
+      Key_Enter,      Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
+                      Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
+      Key_Backspace,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
 
       Key_RightAlt, Key_RightControl, Key_RightGui, Key_Spacebar,
 
